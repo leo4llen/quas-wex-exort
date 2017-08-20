71297ed3e78c0a555ef0fad51786e1d6d744c247
Leo Allen <le.ch4rm@gmail.com>
2017-08-20 10:32:08 +0530

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,70 @@
+const gulp = require('gulp');
+const minify_js = require('gulp-uglify');
+const minify_css = require('gulp-csso');
+const browserSync = require('browser-sync').create();
+const rename = require('gulp-rename');
+const babelify = require('gulp-babel');
+const watch = require('gulp-watch');
+const nodemon = require('gulp-nodemon');
+
+
+
+
+gulp.task('css', () => {
+  return gulp.src('public/stylesheets/style.css')
+            .pipe(minify_css())
+            .pipe(rename('style.min.css'))
+            .pipe(gulp.dest('public/stylesheets'))
+})
+
+
+
+
+gulp.task('js', () => {
+  return gulp.src('public/javascripts/app.js')
+          .pipe(babelify({
+            presets: ['es2015']
+          }))
+          .pipe(minify_js({mangle : true}))
+          .pipe(rename('app.min.js'))
+          .pipe(gulp.dest('public/javascripts'));
+});
+
+
+
+gulp.task('browser-sync', ['nodemon'], () => {
+	browserSync.init(null, {
+		proxy: "http://localhost:3000",
+        files: ["public/**/*.*"],
+        browser: "chrome.exe",
+        port: 1337,
+	});
+  gulp.watch("public/stylesheets/src/*.scss", ['css']).on('change', browserSync.reload);
+
+  gulp.watch("public/javascripts/src/*.js", ['js']).on('change', browserSync.reload);
+
+  gulp.watch("views/**/*.ejs").on('change', browserSync.reload);
+});
+
+
+
+gulp.task('nodemon',  (cb) => {
+
+	let started = false;
+
+	return nodemon({
+    script: 'bin/www',
+    ignore : ['public']
+	}).on('start',  () => {
+		if (!started) {
+			cb();
+			started = true;
+		}
+	});
+});
+
+
+
+gulp.task('build', ['css','js']);
+
+gulp.task('default', ['css',  'js',  'browser-sync']);
