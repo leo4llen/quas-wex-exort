e8a4b2fa1c7b8af44d34f06d9a5b2ba0a1659c48
lech4rm <le.ch4rm@gmail.com>
2017-08-26 13:01:47 +0530

    added orb generation

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -6,15 +6,61 @@ $(document).ready(function(){
   // Initializing some vars
   let gameStarted = false
 
-  $('#start-button').click(() => {
-    gameStarted = true
-    startGame()
+  let orbs  =   ['/images/quas.png' , '/images/wex.png' , '/images/exort.png']
+
+
+  // check state initially
+  $('game-row').css('display' , 'none')
+
+  // Listener for starting up the game
+  $(window).on('keypress' , (e) => {
+    if(!gameStarted){
+      e.which === 32 ? startGame() : console.log(e.which)
+    }
+    else {
+      orbs = changeOrbs (e.key , orbs)
+      setOrbs ( orbs )
+    }
   })
 
 
+
   function startGame () {
-    
+    gameStarted = true;
+    $('#pre-game-row').css('display', 'none')
+    setOrbs (orbs)
   }
 
+  function setOrbs (arr) {
+    $('#game-row ul').html('')
+    arr.forEach((v) => {
+      $('#game-row ul').append(`<li><img src="${v}" alt="" class="img-circle"></li>`)
+    })
+  }
+
+  function changeOrbs (key , arr) {
+    switch(key.toString().toLowerCase()){
+      case 'q':
+        arr.push('/images/quas.png')
+        arr.shift()
+        break
+      case 'w':
+        arr.push('/images/wex.png')
+        arr.shift()
+        break
+      case 'e':
+        arr.push('/images/exort.png')
+        arr.shift()
+        break
+      default:
+        console.log('ggwp')
+        break;
+    }
+    return arr
+  }
+
+  function generateSpell () {
+   return console.log(Math.floor(Math.random() * 10) + 1)
+  }
 
 })
