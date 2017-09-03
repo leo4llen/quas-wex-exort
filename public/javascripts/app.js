7b98c962aa206dbe81d08369e9e70c62cbe71dd1
lech4rm <le.ch4rm@gmail.com>
2017-09-03 19:13:33 +0530

    worked on some stuff

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -5,21 +5,30 @@ $(document).ready(function(){
 
   // Initializing some vars
   let gameStarted = false
-
-  let orbs  =   ['/images/quas.png' , '/images/wex.png' , '/images/exort.png']
-
-
-  // check state initially
+  let orbs
+  let countdown = 2000
+  // Hide game row initially
   $('game-row').css('display' , 'none')
 
-  // Listener for starting up the game
+
+  // Main keypress listener
   $(window).on('keypress' , (e) => {
     if(!gameStarted){
-      e.which === 32 ? startGame() : console.log(e.which)
+      if(e.which === 32){
+        orbs  =   ['quas' , 'wex' , 'exort']
+        $('#pre-game-row').css('display', 'none')
+        $('game-row').css('display', 'block')
+        startGame()
+      }
     }
     else {
-      orbs = changeOrbs (e.key , orbs)
-      setOrbs ( orbs )
+      if(e.key.toString().toLowerCase() === 'r'){
+        spellify( orbs )
+      }
+      else{
+        orbs = changeOrbs (e.key , orbs)
+        setOrbs ( orbs )
+      }
     }
   })
 
@@ -27,40 +36,90 @@ $(document).ready(function(){
 
   function startGame () {
     gameStarted = true;
-    $('#pre-game-row').css('display', 'none')
     setOrbs (orbs)
+    spellify ()
+  }
+
+  function stopGame(){
+    $('#pre-game-row').css('display', 'block')
+    $('#game-row').css('display', 'none')
+    return gameStarted = false
   }
 
   function setOrbs (arr) {
     $('#game-row ul').html('')
     arr.forEach((v) => {
-      $('#game-row ul').append(`<li><img src="${v}" alt="" class="img-circle"></li>`)
+      $('#game-row ul').append(`<li><img src="/images/${v}.png" alt="" class="img-circle"></li>`)
     })
   }
+
+  function spellify (invokedOrbs) {
+    let timer = window.setTimeout(stopGame , countdown)
+    if(invokedOrbs === undefined){
+      let currentSpell = generateSpell ()
+    }
+    else {
+      window.clearTimeout(timer)
+      console.log('timeout cleared')
+    }
+  }
+
 
   function changeOrbs (key , arr) {
     switch(key.toString().toLowerCase()){
       case 'q':
-        arr.push('/images/quas.png')
+        arr.push('quas')
         arr.shift()
         break
       case 'w':
-        arr.push('/images/wex.png')
+        arr.push('wex')
         arr.shift()
         break
       case 'e':
-        arr.push('/images/exort.png')
+        arr.push('exort')
         arr.shift()
         break
       default:
-        console.log('ggwp')
-        break;
+        break
     }
     return arr
   }
 
   function generateSpell () {
-   return console.log(Math.floor(Math.random() * 10) + 1)
+    let spell
+    switch(Math.floor(Math.random() * 10) + 1){
+      case 1:
+        spell = 'cold_snap'
+        break
+      case 2:
+        spell = 'ghost_walk'
+        break
+      case 3:
+       spell = 'ice_wall'
+       break
+      case 4:
+        spell = 'forge_spirits'
+        break
+      case 5:
+        spell = 'tornado'
+        break
+      case 6:
+        spell = 'chaos_meteor'
+        break
+      case 7:
+        spell = 'defeaning_blast'
+        break
+      case 8:
+        spell = 'alacrity'
+        break
+      case 9:
+        spell = 'emp'
+        break
+      case 10:
+        spell = 'sun_strike'
+        break
+    }
+    return spell
   }
 
 })
